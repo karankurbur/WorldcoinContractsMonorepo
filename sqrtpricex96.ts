38cda90b0a57145f3d5cfd52e729f86a4320268f
Karan Kurbur <karankurbur@gmail.com>
2024-01-11 22:52:13 -0800

    deployed need to write docs

--- sqrtpricex96.ts
+++ sqrtpricex96.ts
@@ -1,6 +1,9 @@
 // Use this to determine the pool sqrtPriceX96 when initializing
-const price = 1/46000
-//16992547773297189899623485931520
+
+// 369403212462982385656397824n
+// Resulted in 46000k WBTC for 1 USDC.
+// If USDC is token 0 do 1/46000 else do 46000
+const price = 3
 
 console.log(priceToSqrtPrice(price, 18, 18))
 
