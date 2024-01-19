bd14047bf24dd154299efcc94600c3d6465ddbda
Karan Kurbur <karankurbur@gmail.com>
2024-01-18 16:14:30 -0800

    fix sqrt

--- sqrtpricex96.ts
+++ sqrtpricex96.ts
@@ -4,16 +4,18 @@
 // Resulted in 46000k WBTC for 1 USDC.
 // If USDC is token 0 do 1/46000 else do 46000
 
+const USDC = "0xEC7DF3E5553CC0d5821FB294E43f7203C20EA896"
+const WBTC = "0x4006e60991BfE6aB89cedf3Db3d2ddb8655d582f"
+const WETH = "0x8725756e867DB3C004468ed8ADb73d2EeacD5F3A"
+const WLD = "0x223369abAfBbF7F96c2a17cD7b6d4D6dB03aeEB0"
+
 const fee = 500;
-const USDC = "0xEec4b8B5DE989f3edBA8E3aD23af1E94c5794eac"
-const WBTC = "0x396458af3Bc363B0c41ce1EEbe356Cf8A00F0aef"
-const WETH = "0xf17201f6a5b0658dd7a90468e499ab6cb67e5eb4"
-const WLD = "0x3dc20aaab0bC4a02E38F2b617AcDD5b333F0bf7a"
+
 const OWNER = "0xD3cC83bcd3e7ee71f846b18EAd994603c3b19315"
 
-// calculateSwapRate(USDC, WBTC, 46000)
+calculateSwapRate(USDC, WBTC, 46000)
 // calculateSwapRate(USDC, WETH, 2600)
-calculateSwapRate(USDC, WLD, 3)
+// calculateSwapRate(USDC, WLD, 3)
 
 function calculateSwapRate(USDC: string, OTHER_TOKEN: string, price: number) {
     if(USDC > OTHER_TOKEN) {
@@ -21,17 +23,17 @@ function calculateSwapRate(USDC: string, OTHER_TOKEN: string, price: number) {
             token0: OTHER_TOKEN,
             token1: USDC,
             fee,
-            sqrtPriceX96: priceToSqrtPrice(1/price, 18, 18)
+            sqrtPriceX96: priceToSqrtPrice(price, 18, 18)
         }
 
         const addLiqudity = [
             OTHER_TOKEN,
             USDC,
             fee,
-            "-50000",
-            "50000",
-            "1000000000000000000",
-            "1000000000000000000000000000000000",
+            "-500000",
+            "500000",
+            "100000000000000000000000",
+            "100000000000000000000000000",
             "0",
             "0",
             OWNER,
@@ -45,21 +47,22 @@ function calculateSwapRate(USDC: string, OTHER_TOKEN: string, price: number) {
         console.log(addLiqudity);
     } else {
 
+        console.log("IN HERE")
         const initPool = {
             token0: USDC,
             token1: OTHER_TOKEN,
             fee,
-            sqrtPriceX96: priceToSqrtPrice(price, 18, 18)
+            sqrtPriceX96: priceToSqrtPrice(1/price, 18, 18)
         }
 
         const addLiqudity = [
             USDC,
             OTHER_TOKEN,
             fee,
-            "-50000",
-            "50000",
-            "1000000000000000000000000000000000",
-            "1000000000000000000",
+            "-500000",
+            "500000",
+            "100000000000000000000000000",
+            "100000000000000000000000",
             "0",
             "0",
             OWNER,
